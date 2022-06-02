00b290411913a0df25c285e311b71f5043d99aa5
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-06-02 00:15:33 +0000

    Listing database created and migrated

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,122 @@
+class ListingsController < ApplicationController
+  def index
+    matching_listings = Listing.all
+
+    @list_of_listings = matching_listings.order({ :created_at => :desc })
+
+    render({ :template => "listings/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_listings = Listing.where({ :id => the_id })
+
+    @the_listing = matching_listings.at(0)
+
+    render({ :template => "listings/show.html.erb" })
+  end
+
+  def create
+    the_listing = Listing.new
+    the_listing.owner_id = params.fetch("query_owner_id")
+    the_listing.open_date = params.fetch("query_open_date")
+    the_listing.close_date = params.fetch("query_close_date")
+    the_listing.status = params.fetch("query_status", false)
+    the_listing.address = params.fetch("query_address")
+    the_listing.num_beds = params.fetch("query_num_beds")
+    the_listing.num_baths = params.fetch("query_num_baths")
+    the_listing.num_rooms = params.fetch("query_num_rooms")
+    the_listing.num_beds_offered = params.fetch("query_num_beds_offered")
+    the_listing.num_baths = params.fetch("query_num_baths")
+    the_listing.num_rooms_offered = params.fetch("query_num_rooms_offered")
+    the_listing.region_id = params.fetch("query_region_id")
+    the_listing.state_id = params.fetch("query_state_id")
+    the_listing.city_id = params.fetch("query_city_id")
+    the_listing.neighborhood_id = params.fetch("query_neighborhood_id")
+    the_listing.building_id = params.fetch("query_building_id")
+    the_listing.furnished = params.fetch("query_furnished", false)
+    the_listing.ammenities = params.fetch("query_ammenities")
+    the_listing.pets = params.fetch("query_pets", false)
+    the_listing.smoking = params.fetch("query_smoking", false)
+    the_listing.sublease_category = params.fetch("query_sublease_category")
+    the_listing.gender_pref = params.fetch("query_gender_pref")
+    the_listing.floor = params.fetch("query_floor")
+    the_listing.rent = params.fetch("query_rent")
+    the_listing.dates_flex = params.fetch("query_dates_flex")
+    the_listing.start_flex_left = params.fetch("query_start_flex_left")
+    the_listing.start_flex_right = params.fetch("query_start_flex_right")
+    the_listing.end_flex_left = params.fetch("query_end_flex_left")
+    the_listing.end_flex_right = params.fetch("query_end_flex_right")
+    the_listing.notes = params.fetch("query_notes")
+    the_listing.map_url = params.fetch("query_map_url")
+    the_listing.website_url = params.fetch("query_website_url")
+    the_listing.satellite_url = params.fetch("query_satellite_url")
+    the_listing.num_watching = params.fetch("query_num_watching")
+    the_listing.visibility = params.fetch("query_visibility")
+
+    if the_listing.valid?
+      the_listing.save
+      redirect_to("/listings", { :notice => "Listing created successfully." })
+    else
+      redirect_to("/listings", { :alert => the_listing.errors.full_messages.to_sentence })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_listing = Listing.where({ :id => the_id }).at(0)
+
+    the_listing.owner_id = params.fetch("query_owner_id")
+    the_listing.open_date = params.fetch("query_open_date")
+    the_listing.close_date = params.fetch("query_close_date")
+    the_listing.status = params.fetch("query_status", false)
+    the_listing.address = params.fetch("query_address")
+    the_listing.num_beds = params.fetch("query_num_beds")
+    the_listing.num_baths = params.fetch("query_num_baths")
+    the_listing.num_rooms = params.fetch("query_num_rooms")
+    the_listing.num_beds_offered = params.fetch("query_num_beds_offered")
+    the_listing.num_baths = params.fetch("query_num_baths")
+    the_listing.num_rooms_offered = params.fetch("query_num_rooms_offered")
+    the_listing.region_id = params.fetch("query_region_id")
+    the_listing.state_id = params.fetch("query_state_id")
+    the_listing.city_id = params.fetch("query_city_id")
+    the_listing.neighborhood_id = params.fetch("query_neighborhood_id")
+    the_listing.building_id = params.fetch("query_building_id")
+    the_listing.furnished = params.fetch("query_furnished", false)
+    the_listing.ammenities = params.fetch("query_ammenities")
+    the_listing.pets = params.fetch("query_pets", false)
+    the_listing.smoking = params.fetch("query_smoking", false)
+    the_listing.sublease_category = params.fetch("query_sublease_category")
+    the_listing.gender_pref = params.fetch("query_gender_pref")
+    the_listing.floor = params.fetch("query_floor")
+    the_listing.rent = params.fetch("query_rent")
+    the_listing.dates_flex = params.fetch("query_dates_flex")
+    the_listing.start_flex_left = params.fetch("query_start_flex_left")
+    the_listing.start_flex_right = params.fetch("query_start_flex_right")
+    the_listing.end_flex_left = params.fetch("query_end_flex_left")
+    the_listing.end_flex_right = params.fetch("query_end_flex_right")
+    the_listing.notes = params.fetch("query_notes")
+    the_listing.map_url = params.fetch("query_map_url")
+    the_listing.website_url = params.fetch("query_website_url")
+    the_listing.satellite_url = params.fetch("query_satellite_url")
+    the_listing.num_watching = params.fetch("query_num_watching")
+    the_listing.visibility = params.fetch("query_visibility")
+
+    if the_listing.valid?
+      the_listing.save
+      redirect_to("/listings/#{the_listing.id}", { :notice => "Listing updated successfully."} )
+    else
+      redirect_to("/listings/#{the_listing.id}", { :alert => the_listing.errors.full_messages.to_sentence })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_listing = Listing.where({ :id => the_id }).at(0)
+
+    the_listing.destroy
+
+    redirect_to("/listings", { :notice => "Listing deleted successfully."} )
+  end
+end
---
+++ db/migrate/20220602001347_create_listings.rb
@@ -0,0 +1,43 @@
+class CreateListings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :listings do |t|
+      t.integer :owner_id
+      t.date :open_date
+      t.date :close_date
+      t.boolean :status
+      t.string :address
+      t.integer :num_beds
+      t.integer :num_baths
+      t.integer :num_rooms
+      t.integer :num_beds_offered
+      t.offered :num_baths
+      t.integer :num_rooms_offered
+      t.integer :region_id
+      t.integer :state_id
+      t.integer :city_id
+      t.integer :neighborhood_id
+      t.integer :building_id
+      t.boolean :furnished
+      t.string :ammenities
+      t.boolean :pets
+      t.boolean :smoking
+      t.integer :sublease_category
+      t.integer :gender_pref
+      t.integer :floor
+      t.currency :rent
+      t.integer :dates_flex
+      t.integer :start_flex_left
+      t.integer :start_flex_right
+      t.integer :end_flex_left
+      t.integer :end_flex_right
+      t.string :notes
+      t.string :map_url
+      t.string :website_url
+      t.string :satellite_url
+      t.integer :num_watching
+      t.integer :visibility
+
+      t.timestamps
+    end
+  end
+end
